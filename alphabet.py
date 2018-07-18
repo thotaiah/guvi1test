7e3ec75993ee231a1fc1596290a999f431cf3802
thotaiah <41178205+thotaiah@users.noreply.github.com>
2018-07-18 06:06:02 +0000

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    char c;
+    char t;
     printf("Enter a character: ");
-    scanf("%c",&c);
+    scanf("%c",&t);
 
-    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
+    if( (t>='A' && t<='Z') || (t>='a' && t<='z'))
+        printf("%c is an alphabet.",t);
     else
-        printf("%c is not an alphabet.",c);
+        printf("%c is not an alphabet.",t);
 
     return 0;
 }
